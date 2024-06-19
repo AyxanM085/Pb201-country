fcedb86a87c9e26f433f61f1c36b5f2e25fbdb3b
AyxanM085 <Aykhankhm@code.edu.az>
2024-06-19 11:05:27 +0400

    Add files via upload

---
+++ Project/Project/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Weapon weapon = new Weapon(30, 10, "tekli");
+
+        while (true)
+        {
+            Console.WriteLine("0 - İnformasiya almaq üçün");
+            Console.WriteLine("1 - Shoot metodu üçün");
+            Console.WriteLine("2 - Fire metodu üçün");
+            Console.WriteLine("3 - GetRemainBulletCount metodu üçün");
+            Console.WriteLine("4 - Reload metodunu çağırmaq üçün");
+            Console.WriteLine("5 - ChangeFireMode metodu üçün");
+            Console.WriteLine("6 - Proqramdan dayandırmaq üçün");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "0")
+            {
+                Console.WriteLine($"Atış modu: {weapon.GetFireMode()}");
+                Console.WriteLine($"Qalan gülle sayı: {weapon.GetRemainBulletCount()}");
+            }
+            else if (choice == "1")
+            {
+                weapon.Shoot();
+            }
+            else if (choice == "2")
+            {
+                weapon.Fire();
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine($"Qalan gülle sayı: {weapon.GetRemainBulletCount()}");
+            }
+            else if (choice == "4")
+            {
+                weapon.Reload();
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Yeni atış modunu daxil edin:");
+                string newFireMode = Console.ReadLine();
+                weapon.ChangeFireMode(newFireMode);
+            }
+            else if (choice == "6")
+            {
+                break; // while dövründən çıxmaq üçün
+            }
+            else
+            {
+                Console.WriteLine("Düzgün seçim edin.");
+            }
+        }
+    }
+}
---
+++ Project/Project/Weapon.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class Weapon
+{
+    private int bulletCount;
+    private int maxBulletCapacity;
+    private string fireMode;
+
+    public Weapon(int maxBulletCapacity, int initialBulletCount, string initialFireMode)
+    {
+        this.maxBulletCapacity = maxBulletCapacity;
+        this.bulletCount = initialBulletCount;
+        this.fireMode = initialFireMode;
+    }
+
+    public void Shoot()
+    {
+        if (bulletCount > 0)
+        {
+            Console.WriteLine("Gülle atıldı!");
+            bulletCount--;
+        }
+        else
+        {
+            Console.WriteLine("Daraqda gülle qalmayıb!");
+        }
+    }
+
+    public void Fire()
+    {
+        if (bulletCount > 0)
+        {
+            Console.WriteLine($"Bütün gülleler atəşləndi! {bulletCount} gülle qalıb.");
+            bulletCount = 0;
+        }
+        else
+        {
+            Console.WriteLine("Daraqda gülle qalmayıb!");
+        }
+    }
+
+    public int GetRemainBulletCount()
+    {
+        return bulletCount;
+    }
+
+    public void Reload()
+    {
+        bulletCount = maxBulletCapacity;
+        Console.WriteLine($"Daraq yenidən dolduruldu. {bulletCount} gülle var.");
+    }
+
+    public void ChangeFireMode(string newFireMode)
+    {
+        fireMode = newFireMode;
+        Console.WriteLine($"Atış modu dəyişdirildi: {fireMode}");
+    }
+
+    public string GetFireMode()
+    {
+        return fireMode;
+    }
+}
